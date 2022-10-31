1c9977b4cd55db7bb5587d5673413a20e61d3fb1
Edgardo Vaca <edgardovac2298@gmail.com>
2022-10-31 15:03:19 -0500

    listIssue

--- src/issue/issue.controller.ts
+++ src/issue/issue.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { IssueService } from './issue.service';
 import { CreateIssueDto } from './dto/create-issue.dto';
 
@@ -6,8 +6,13 @@ import { CreateIssueDto } from './dto/create-issue.dto';
 export class IssueController {
   constructor(private readonly issueService: IssueService) { }
 
-  @Post()
+  @Post('/new')
   createIssue(@Body() createIssueDto: CreateIssueDto) {
     return this.issueService.createIssue(createIssueDto);
+  }
+
+  @Get('/list')
+  listIssue() {
+    return this.issueService.listIssue();
   }
 }
--- src/issue/issue.service.ts
+++ src/issue/issue.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateIssueDto } from './dto/create-issue.dto';
 import { v4 as uuid } from "uuid";
 import { DatabaseService } from '../db/db.service';
@@ -36,6 +36,27 @@ export class IssueService {
         errorMessage: `ERROR issue`,
         detail: `ERROR createIssue function`
       };
+    }
+  }
+
+  async listIssue() {
+    try {
+      return {
+        message: 'Retrieved successfully',
+        data: await this.dbService
+          .connect()
+          .query({
+            TableName: this.TABLE_NAME,
+            IndexName: "verify-index",
+            KeyConditionExpression: "verify = :v_solution",
+            ExpressionAttributeValues: {
+              ":v_solution": "no"
+            },
+          })
+          .promise(),
+      };
+    } catch (err) {
+      throw new InternalServerErrorException(err);
     }
   }
 }
